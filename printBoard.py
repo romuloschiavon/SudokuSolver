47bb1e25bb2c28840d7655bd26f910c699a996a2
romulo <schiavonromulo@gmail.com>
2021-12-21 18:05:43 -0300

    Finished printing in cmd

--- printBoard.py
+++ printBoard.py
@@ -1,7 +1,30 @@
 from board import *
 
+class Impressao():
+    def __init__(self, base):
+        self.base = base
+        self.board = Board(base=self.base, print=False).board
+        
+    def imprimir(self):
+        Linha = " #########################"
+        i = 0
+        j = 0
+        print("\n" + Linha, end="\n")
+        for line in self.board:
+            print(" ", end="")
+            print("# ", end="")
+            for value in line:
+                i += 1
+                if i == 4 or i == 7:
+                    print("# ", end="")
+                print(value, end=" ")
+            print("#", end="\n")
+            if j == 2 or j == 5:
+                print(Linha)
+            i = 0
+            j += 1
+        print(Linha, end="\n")
 
-Tabuleiro = Board(base=3, print=False).board
+numero = int(input("Digite a base do tabuleiro: "))
 
-for line in Tabuleiro:
-    print(line)
+Impressao(base=numero).imprimir()
